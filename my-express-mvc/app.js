f54ac6c984a41d8dd129f8df3e2409b17b5344ab
Valens Florencia <valensflorencia_2226240038@mhs.mdp.ac.id>
2024-10-18 18:33:15 +0700

    Building a data model with mongtoDB
    belajar mongodb

--- my-express-mvc/app.js
+++ my-express-mvc/app.js
@@ -8,6 +8,7 @@ require('./app_server/models/db');
 
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
+var mahasiswasRouter = require('./app_server/routes/mahasiswas');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api', mahasiswasRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
