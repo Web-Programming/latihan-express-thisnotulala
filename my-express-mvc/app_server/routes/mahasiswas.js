c26b1cd23e9a62589e49f2d93668ba187878987b
someone <>
2024-10-25 17:16:33 +0700

    belajar API
    belajar API

--- my-express-mvc/app_server/routes/mahasiswas.js
+++ my-express-mvc/app_server/routes/mahasiswas.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const mahasiswaController = require("../controllers/controllerMahasiswa");
-//Fungsi dan Rute Index Kita Gunakan Untuk Memanggil Semua Data Dalam Database MongoDB
+// Fungsi dan Rute Index Kita Gunakan Untuk Memanggil Semua Data Dalam Database MongoDB
 router.get("/mahasiswa", mahasiswaController.Index);
+router.post("/mahasiswa/insert", mahasiswaController.insert);
 module.exports = router;
