9ecea7637fd3bd9376e15bcc91ea45e3c3ecdb01
Cesya Catherine <cesyacatherine_2226240018@mhs.mdp.ac.id>
2024-11-22 18:35:14 +0700

    commit
    commit

--- belajar_angular/src/app/housing-location/housing-location.component.ts
+++ belajar_angular/src/app/housing-location/housing-location.component.ts
@@ -8,7 +8,7 @@ import { RouterModule } from '@angular/router';
   imports: [CommonModule, RouterModule],
   template: `
     <section class="listing">
-      <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{ housingLocation.name }}">
+      <img class="listing-photo" [src]="baseUrl + housingLocation.photo" alt="Exterior photo of {{ housingLocation.name }}">
       <h2 class="listing-heading">{{ housingLocation.name }}</h2>
       <p class="listing-location">{{ housingLocation.city }}, {{ housingLocation.state }}</p>
       <a [routerLink]="['details', housingLocation.id]">Show More</a>
@@ -18,4 +18,5 @@ import { RouterModule } from '@angular/router';
 })
 export class HousingLocationComponent {
   @Input() housingLocation!: HousingLocation
+  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa/'; 
 }
